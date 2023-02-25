6061eca95d819931fe1b6d142ccfc2127c7e263e
fotiecodes <42372656+FotieMConstant@users.noreply.github.com>
2023-02-25 13:00:31 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,10 +9,26 @@ module.exports = {
     "plugin:prettier/recommended",
   ],
   parserOptions: {
+    requireConfigFile: false,
     parser: "@babel/eslint-parser",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // we want to force semicolons
+    semi: ["error", "always"],
+    indent: ["error", 2],
+    "vue/multi-word-component-names": [
+      "error",
+      {
+        ignores: ["default"],
+      },
+    ],
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
   },
 };
