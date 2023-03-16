49e067ee377e28b38cb36b277d0be765f645b349
fotiemconstant <fotiemoghommahieconstant@gmail.com>
2023-03-16 23:00:24 +0300

    🛠check for updates without refreshing app

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <nav>
     <LocalLang />
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About </router-link>
+    <router-link to="/about">About</router-link>
   </nav>
   <router-view />
   <!-- set progressbar -->
