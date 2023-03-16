1de7aad44575b44d8443541a044148f6ac1bea93
fotiemconstant <fotiemoghommahieconstant@gmail.com>
2023-03-16 22:39:07 +0300

    🛠fixed minor bugs and installed more dev dep
    install scss to project, configure a notification for service workers,  add vue-progressbar, app.use(i18n);

--- src/main.js
+++ src/main.js
@@ -3,18 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./index.css";
-import { createI18n } from "vue-i18n";
-import loadLocaleMessages from "./i18nHelpers";
+import { i18n } from "./i18n";
 import "./registerServiceWorker";
 import { VueProgressBar, options } from "./plugins/VueProgressBarPlugin";
-
-// i18n configuration
-const i18n = createI18n({
-  // something vue-i18n options here ...
-  locale: process.env.VUE_APP_I18N_LOCALE || "en",
-  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || "en",
-  messages: loadLocaleMessages(),
-});
 
 const app = createApp(App);
 app.use(store);
